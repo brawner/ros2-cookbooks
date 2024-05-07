c98b1dcc3e6098110257a79f0d3add2da349d407
Chris Lalancette <clalancette@gmail.com>
2024-05-07 14:58:59 -0400

    Only do openssl stuff from cached for older versions.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- cookbooks/ros2_windows/recipes/openssl.rb
+++ cookbooks/ros2_windows/recipes/openssl.rb
@@ -1,38 +1,30 @@
 openssl_versions = {
   "humble" => "1_1_1L",
   "iron" => "1_1_1L",
-  "jazzy" => "1_1_1L",
-  "rolling" => "1_1_1L",
+  "jazzy" => nil,  # OpenSSL is installed via choco
+  "rolling" => nil,  # OpenSSL is installed via choco
 }.freeze
 
 openssl_version = openssl_versions[node["ros2_windows"]["ros_distro"]]
 
-# The default installation location changed for the 1.1.1 releases. However if
-# 1.0.2 was installed previously then some state persists and a 1.1.1
-# installation will use the 1.0.2 location. To be correct in every situation
-# we would either need to specifiy the installation path for whatever we
-# install or retrieve whatever it is before configuring.
-openssl_conf_dir = if openssl_version == "1_0_2u"
-                     'C:\\OpenSSL-Win64'
-                   else
-                     'C:\\Program Files\\OpenSSL-Win64'
-                   end
+if !openssl_version.nil?
+  openssl_conf_dir = 'C:\\Program Files\\OpenSSL-Win64'
 
+  windows_package 'openssl' do
+    source "https://ftp.osuosl.org/pub/ros/download.ros.org/downloads/openssl/Win64OpenSSL-#{openssl_version}.exe"
+    options '/VERYSILENT'
+  end
 
-windows_package 'openssl' do
-  source "https://ftp.osuosl.org/pub/ros/download.ros.org/downloads/openssl/Win64OpenSSL-#{openssl_version}.exe"
-  options '/VERYSILENT'
-end
+  windows_env 'OPENSSL_CONF' do
+    key_name 'OPENSSL_CONF'
+    value "#{openssl_conf_dir}\\bin\\openssl.cfg"
+    action :create
+  end
 
-windows_env 'OPENSSL_CONF' do
-  key_name 'OPENSSL_CONF'
-  value "#{openssl_conf_dir}\\bin\\openssl.cfg"
-  action :create
-end
-
-windows_env 'PATH' do
-  key_name 'PATH'
-  value "#{openssl_conf_dir}\\bin"
-  delim ';'
-  action :modify
+  windows_env 'PATH' do
+    key_name 'PATH'
+    value "#{openssl_conf_dir}\\bin"
+    delim ';'
+    action :modify
+  end
 end
