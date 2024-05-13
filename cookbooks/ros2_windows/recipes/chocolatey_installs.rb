bbb1eefac6d8cd464efdc925f04b854b4989bb41
Chris Lalancette <clalancette@gmail.com>
2024-05-13 16:05:40 -0400

    Switch to using OpenSSL from Chocolatey. (#68)
    * Update to newer CMake version for Jazzy and Rolling.
    
    * Switch to a single list for all chocolatey versions.
    
    * Do all openssl installs via chocolatey.
    
    * Remove openssl from recipe list.
    
    * Switch no versions to 'nil'.
    
    * Fix openssl version for humble and iron.
    
    * Try OpenSSL 1.1.1.2100 on Jazzy and Rolling.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- cookbooks/ros2_windows/recipes/chocolatey_installs.rb
+++ cookbooks/ros2_windows/recipes/chocolatey_installs.rb
@@ -1,5 +1,49 @@
-%w[git curl vcredist2013 vcredist140 patch winflexbison3].each do |pkg|
-  chocolatey_package pkg do
+choco_packages_and_versions = {
+  "humble" => {
+    "cmake" => "3.24.3",
+    "cppcheck" => "1.90",
+    "curl" => nil,
+    "git" => nil,
+    "openssl" => "1.1.1.2100",
+    "vcredist140" => nil,
+    "vcredist2013" => nil,
+    "winflexbison3" => nil,
+  },
+  "iron" => {
+    "cmake" => "3.24.3",
+    "cppcheck" => "1.90",
+    "curl" => nil,
+    "git" => nil,
+    "openssl" => "1.1.1.2100",
+    "vcredist140" => nil,
+    "vcredist2013" => nil,
+    "winflexbison3" => nil,
+  },
+  "jazzy" => {
+    "cmake" => "3.29.2",
+    "cppcheck" => "1.90",
+    "curl" => nil,
+    "git" => nil,
+    "openssl" => "1.1.1.2100",
+    "vcredist140" => nil,
+    "vcredist2013" => nil,
+    "winflexbison3" => nil,
+  },
+  "rolling" => {
+    "cmake" => "3.29.2",
+    "cppcheck" => "1.90",
+    "curl" => nil,
+    "git" => nil,
+    "openssl" => "1.1.1.2100",
+    "vcredist140" => nil,
+    "vcredist2013" => nil,
+    "winflexbison3" => nil,
+  },
+}.freeze
+
+choco_packages_and_versions[node["ros2_windows"]["ros_distro"]].each do |pkgname, version|
+  chocolatey_package pkgname do
+    version version
     options "--debug"
     list_options "--debug"
     retries 20
@@ -7,23 +51,17 @@
   end
 end
 
-chocolatey_package 'cmake' do
-  version '3.24.3'
-  options "--debug"
-  list_options "--debug"
-  retries 20
-  retry_delay 10
-end
-
-chocolatey_package 'cppcheck' do
-  version '1.90'
-end
-
 windows_env 'PATH' do
   key_name 'PATH'
-  value 'C:\\Program Files\\Git\\cmd;C:\\Program Files\\CMake\\bin'
+  value 'C:\\Program Files\\Git\\cmd;C:\\Program Files\\CMake\\bin;C:\\Program Files\\OpenSSL-Win64\\bin'
   delim ';'
   action :modify
+end
+
+windows_env 'OPENSSL_CONF' do
+  key_name 'OPENSSL_CONF'
+  value "C:\\Program Files\\OpenSSL-Win64\\bin\\openssl.cfg"
+  action :create
 end
 
 custom_chocolatey_packages = {
