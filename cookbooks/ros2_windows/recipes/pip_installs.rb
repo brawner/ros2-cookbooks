96cbbebd449800d20e3db58716a3c2271c55c18d
Chris Lalancette <clalancette@gmail.com>
2024-12-13 17:08:17 -0500

    Pin EmPy to <4. (#78)
    * Pin EmPy to < 4.
    
    * Add additional quotes.
    
    That's because < is a special character on Windows,
    and we need to escape it.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- cookbooks/ros2_windows/recipes/pip_installs.rb
+++ cookbooks/ros2_windows/recipes/pip_installs.rb
@@ -24,7 +24,7 @@ required_pip_packages = %w[
   colcon-test-result
   colcon-zsh
   cryptography
-  EmPy
+  EmPy<4
   fastjsonschema==2.19.0
   jsonschema
   lark-parser
@@ -88,7 +88,7 @@ end
 
 execute 'pip_required' do
   command lazy {
-    "#{node.run_state[:python_dir]}\\python.exe -m pip install -U #{required_pip_packages.join(' ')}"
+    "#{node.run_state[:python_dir]}\\python.exe -m pip install -U \"#{required_pip_packages.join('" "')}\""
   }
 end
 
